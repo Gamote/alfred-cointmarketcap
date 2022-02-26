00fd836063bce98a59402a6b2395f6924531f92f
David Gamote <contact@gamote.ro>
2022-02-26 18:42:03 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,172 @@
+interface Response {
+  data: {
+    id: number;
+    symbol: string;
+    name: string;
+    amount: number;
+    quote: {
+      GBP: {
+        price: number;
+        last_updated: Date;
+      };
+    };
+  };
+}
+
+interface Result {
+  title: string;
+  subtitle: string;
+  arg?: string;
+  text?: {
+    copy: string;
+    largetype: string;
+  };
+  mods?: {
+    cmd: {
+      arg: string;
+      subtitle: string;
+    };
+  };
+}
+
+interface Input {
+  apiKey: string;
+  value: number;
+  coinSource: string;
+  defaultCoinOutput?: string;
+  coinOutput: string;
+}
+
+const MISSING_ARGUMENT_MESSAGE = Object.freeze({
+  title: `Incorrect argument`,
+  subtitle: `Example: cmc 0.02 BTC GBP`,
+});
+
+const ERROR_MESSAGE = Object.freeze({
+  title: `Incorrect request`,
+  subtitle: `Are you sure thi coin exists? How about your API key?`,
+});
+
+const newError = (e?: unknown) => {
+  return {
+    title: `Incorrect request`,
+    subtitle: `${JSON.stringify(e)}`,
+  }
+}
+
+const printResult = (result: Result) =>
+  console.log(`${JSON.stringify({ items: [result] })}`);
+
+/**
+ * Parse the query into an {@link Input} type
+ */
+const parseQuery = (): Input => {
+  const args = Deno.args[0].split(" ");
+
+  // Checks the arguments
+  if (!Array.isArray(args)) {
+    throw new Error("Invalid args");
+  }
+
+  // Get the CoinMarketCap API key
+  let apiKey = args[0];
+  if (!apiKey) {
+    throw new Error("Invalid CoinMarketCap API key");
+  }
+  args.splice(0, 1);
+
+  // Get the default coin output
+  let defaultCoinOutput = args[0];
+  if (defaultCoinOutput) {
+    args.splice(0, 1);
+  }
+
+  // Determine the value
+  let value = Number(args[0]);
+  if (isNaN(value)) {
+    value = 1;
+  } else {
+    args.splice(0, 1);
+  }
+
+  // Determine the coin source
+  let coinSource = args[0];
+  if (!coinSource) {
+    throw new Error("Invalid coin source");
+  }
+
+  // Determine the coin output
+  let coinOutput = args[1];
+  if (!coinOutput) {
+    coinOutput = defaultCoinOutput ?? "USD";
+  }
+
+  return {
+    apiKey,
+    value,
+    coinSource,
+    defaultCoinOutput,
+    coinOutput,
+  }
+}
+
+/**
+ * Entry point
+ */
+const main = async () => {
+  let input: Input;
+
+  // Parse input
+  try {
+    input = parseQuery();
+  } catch (e) {
+    printResult({
+      title: `Incorrect request`,
+      // subtitle: `Error: ${JSON.stringify(e.message)}`,
+      subtitle: `Error: ${JSON.stringify(Deno.args)}`,
+    });
+    return;
+  }
+
+  const { apiKey, value, coinSource, coinOutput } = input;
+
+  // Uppercase values
+  const coinSourceUpperCase = coinSource.toUpperCase();
+  const coinTargetUpperCase = coinOutput.toUpperCase();
+
+  //
+  try {
+    const response = await fetch(
+      `https://pro-api.coinmarketcap.com/v1/tools/price-conversion?amount=${value}&symbol=${coinSourceUpperCase}&convert=${coinTargetUpperCase}`,
+      {
+        headers: {
+          "X-CMC_PRO_API_KEY": apiKey,
+        },
+      }
+    );
+    const { quote, name: coinSourceName } = (await response.json()).data;
+    const result = quote[coinTargetUpperCase].price;
+    const coinSourceNameUrl = coinSourceName.toLowerCase().replace(/ /g, "-");
+
+    printResult({
+      title: result,
+      subtitle: `${value} ${coinSourceUpperCase} = ${result} ${coinTargetUpperCase}`,
+      arg: result,
+      text: {
+        copy: result,
+        largetype: `${value} ${coinSourceUpperCase} = ${result} ${coinTargetUpperCase}`,
+      },
+      mods: {
+        cmd: {
+          arg: `https://coinmarketcap.com/currencies/${coinSourceNameUrl}/`,
+          subtitle: `Open ${coinSourceName} on CoinMarketCap`,
+        },
+      },
+    });
+  } catch(e) {
+    // printResult(ERROR_MESSAGE);
+    printResult(newError(`${e.message} - ${apiKey}`));
+  }
+}
+
+void main();
